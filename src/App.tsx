f9d6704f1eabdcc86ab6816f64e9610ceed6cd5b
TinyMotor <TinyMotor@outlook.com>
2026-06-30 15:14:46 +0800

    chore: 配置项目支持github pages部署
    1.  替换BrowserRouter为HashRouter适配gh-pages部署
    2.  修正index.html中favicon的相对路径
    3.  添加vite base配置项
    4.  新增gh-pages依赖和部署脚本

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Workbench from "@/pages/Workbench";
 
 export default function App() {
